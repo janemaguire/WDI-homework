003fa31d61bebeb156c39fab920f6686e811136e
Jane Maguire <jane.m.maguire@gmail.com>
2016-09-28 18:08:28 +0100

    Added prompts for user

---
+++ janemaguire/w01d03/calculator/js/app.js
@@ -0,0 +1,5 @@
+console.log("js loaded");
+
+var userFirstNumber = prompt("What is your first number?");
+var userAction = prompt("What action would you like to take?");
+var userSecondNumber = prompt ("What is your second number?");
