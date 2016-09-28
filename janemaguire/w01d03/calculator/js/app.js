ad66cc77cefa4f6ffc2a50c9bcfe64105679fcab
Jane Maguire <jane.m.maguire@gmail.com>
2016-09-28 18:36:25 +0100

    Added switch statement for actions

--- janemaguire/w01d03/calculator/js/app.js
+++ janemaguire/w01d03/calculator/js/app.js
@@ -1,5 +1,27 @@
 console.log("js loaded");
 
 var userFirstNumber = prompt("What is your first number?");
-var userAction = prompt("What action would you like to take?");
+var userAction = prompt("What action would you like to take? Choose from + , - , / or * . ");
 var userSecondNumber = prompt ("What is your second number?");
+
+switch(userAction) {
+
+  case "+":
+  console.log(parseFloat(userFirstNumber) + parseFloat(userSecondNumber));
+  break;
+
+  case "-":
+  console.log(parseFloat(userFirstNumber) - parseFloat(userSecondNumber));
+  break;
+
+  case "/":
+  console.log(parseFloat(userFirstNumber) / parseFloat(userSecondNumber));
+  break;
+
+  case "*":
+  console.log(parseFloat(userFirstNumber) * parseFloat(userSecondNumber));
+  break;
+
+  default:
+  console.log("Please choose an action from the list!");
+}
