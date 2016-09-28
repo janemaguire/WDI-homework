75f930e98da5c9c17141a94c996235d52af954d5
Jane Maguire <jane.m.maguire@gmail.com>
2016-09-28 19:03:45 +0100

    Tried to add square root without asking for second number

--- janemaguire/w01d03/calculator/js/app.js
+++ janemaguire/w01d03/calculator/js/app.js
@@ -1,8 +1,15 @@
 console.log("js loaded");
 
 var userFirstNumber = prompt("What is your first number?");
-var userAction = prompt("What action would you like to take? Choose from + , - , / , * , p (to the power of). ");
-var userSecondNumber = prompt ("What is your second number?");
+var userAction = prompt("What action would you like to take? Choose from + , - , / , * , p (to the power of) , s (square root of). ");
+
+if (userAction === s) {
+  console.log(Math.sqrt(parseFloat(userFirstNumber)));
+}
+
+else {
+  var userSecondNumber = prompt ("What is your second number?");
+}
 
 switch(userAction) {
 
@@ -24,6 +31,7 @@ switch(userAction) {
 
   case "p":
   console.log(Math.pow(parseFloat(userFirstNumber), parseFloat(userSecondNumber)));
+  break;
 
   default:
   console.log("Please choose an action from the list!");
