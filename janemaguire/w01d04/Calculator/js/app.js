16912b9540ecce065450921dcb0be4037bb45882
Jane Maguire <jane.m.maguire@gmail.com>
2016-09-29 18:32:56 +0100

    Added event and switch statement

--- janemaguire/w01d04/Calculator/js/app.js
+++ janemaguire/w01d04/Calculator/js/app.js
@@ -1,35 +1,30 @@
 console.log("yay");
 
-var input1 = document.getElementById("input1");
-var operation = document.getElementById("operation");
-var input2 = document.getElementById("input2");
+document.getElementById("button").addEventListener("click", calculate);
 
-// switch(userAction) {
-//
-//   case "+":
-//   alert("The answer is " + (parseFloat(userFirstNumber) + parseFloat(userSecondNumber)));
-//   break;
-//
-//   case "-":
-//   alert("The answer is " + (parseFloat(userFirstNumber) - parseFloat(userSecondNumber)));
-//   break;
-//
-//   case "/":
-//   alert("The answer is " + (parseFloat(userFirstNumber) / parseFloat(userSecondNumber)));
-//   break;
-//
-//   case "*":
-//   alert("The answer is " + (parseFloat(userFirstNumber) * parseFloat(userSecondNumber)));
-//   break;
-//
-//   case "p":
-//   alert("The answer is " + (Math.pow(parseFloat(userFirstNumber), parseFloat(userSecondNumber))));
-//   break;
-//
-//   case "s":
-//   alert("The answer is " + (Math.sqrt(parseFloat(userFirstNumber))));
-//   break;
-//
-//   default:
-//   alert("Please choose an action from the list!");
-// }
+function calculate(e) {
+  e.preventDefault();
+  var input1 = document.getElementById("input1").value;
+  var operator = document.getElementById("operator").value;
+  var input2 = document.getElementById("input2").value;
+
+switch (operator) {
+
+  case "+":
+  console.log((parseFloat(input1)) + parseFloat(input2));
+  break;
+
+  case "-":
+  console.log((parseFloat(input1)) - parseFloat(input2));
+  break;
+
+  case "*":
+  console.log((parseFloat(input1)) * parseFloat(input2));
+  break;
+
+  case "/":
+  console.log((parseFloat(input1)) / parseFloat(input2));
+  break;
+}
+
+}
