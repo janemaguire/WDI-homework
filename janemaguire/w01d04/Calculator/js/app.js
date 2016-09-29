a194325d3572b7b9a5f0aa897d2ba08692e2acd4
Jane Maguire <jane.m.maguire@gmail.com>
2016-09-29 18:00:18 +0100

    Laying out HTML

---
+++ janemaguire/w01d04/Calculator/js/app.js
@@ -0,0 +1,35 @@
+console.log("yay");
+
+var input1 = document.getElementById("input1");
+var operation = document.getElementById("operation");
+var input2 = document.getElementById("input2");
+
+// switch(userAction) {
+//
+//   case "+":
+//   alert("The answer is " + (parseFloat(userFirstNumber) + parseFloat(userSecondNumber)));
+//   break;
+//
+//   case "-":
+//   alert("The answer is " + (parseFloat(userFirstNumber) - parseFloat(userSecondNumber)));
+//   break;
+//
+//   case "/":
+//   alert("The answer is " + (parseFloat(userFirstNumber) / parseFloat(userSecondNumber)));
+//   break;
+//
+//   case "*":
+//   alert("The answer is " + (parseFloat(userFirstNumber) * parseFloat(userSecondNumber)));
+//   break;
+//
+//   case "p":
+//   alert("The answer is " + (Math.pow(parseFloat(userFirstNumber), parseFloat(userSecondNumber))));
+//   break;
+//
+//   case "s":
+//   alert("The answer is " + (Math.sqrt(parseFloat(userFirstNumber))));
+//   break;
+//
+//   default:
+//   alert("Please choose an action from the list!");
+// }
