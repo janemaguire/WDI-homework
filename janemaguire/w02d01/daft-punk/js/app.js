d6554eca37383f51d3ea31c44368323d0599a229
Jane Maguire <jane.m.maguire@gmail.com>
2016-10-03 21:38:51 +0100

    Got lost in loops! Went back to basics tot ry and get first button working

--- janemaguire/w02d01/daft-punk/js/app.js
+++ janemaguire/w02d01/daft-punk/js/app.js
@@ -1,23 +1,26 @@
-var buttons = document.querySelectorAll("li");
+// var buttons = document.querySelectorAll("li");
+// var sounds = document.getElementsByClassName("tile");
+//
+// for (var i=0; i<buttons.length; i++) {
+//   buttons[i].addEventListener("click", function() {
+//     this.play(innerHTML);
+//   });
+// }
+//
+// var sounds = document.getElementsByClassName("tile"); {
+//   for (var i=0; i<sounds.length; i++) {
+//     console.log(sounds[i].innerHTML);
+//   }
+// }
 
-for (var i=0; i<buttons.length; i++) {
-  buttons[i].addEventListener("click", function() {
-    console.log(sounds);
+// Put event listener on first tile and play correct music
+
+var work_it = document.getElementById('work_it');
+var button = document.getElementById('work_it');
+
+button.addEventListener("click", function() {
+
+work_it.src = "/sounds/work_it.wav";
+document.work_it.play();
+
   });
-}
-
-var sounds = document.getElementsByClassName("tile"); {
-  for (var i=0; i<sounds.length; i++) {
-    console.log(sounds[i].innerHTML);
-  }
-}
-//
-// var noiseMaker = document.getElementById('noise-maker');
-// var button = document.getElementById('play-button');
-//
-// button.addEventListener("click", function() {
-//
-// noiseMaker.src = "https://upload.wikimedia.org/wikipedia/commons/e/ef/Eastern_Whipbird.ogg";
-// noiseMaker.play();
-//
-// });
