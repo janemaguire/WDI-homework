bfbf59ec6fbbd737f9e65bceca1f46eb462fe205
Jane Maguire <jane.m.maguire@gmail.com>
2016-10-04 18:30:37 +0100

    Moved computer choice to inside event listeners, incremented score

--- janemaguire/w02d02/rock-paper-scissors/js/app.js
+++ janemaguire/w02d02/rock-paper-scissors/js/app.js
@@ -1,44 +1,68 @@
 document.addEventListener("DOMContentLoaded", function () {
-
+  var status = document.getElementById("status");
   var rockButton = document.getElementById("rock");
   var paperButton = document.getElementById("paper");
   var scissorsButton = document.getElementById("scissors");
   var userChoice = "";
   var choices = ["rock", "paper", "scissors"];
-  var randomIndex = Math.floor(Math.random() * choices.length);
-  var compChoice = choices[randomIndex];
+  var compChoice = "";
+  var userScore = 0;
+  var compScore = 0;
+  var userScoreBoard = document.getElementById('humanScore');
+  var computerScoreBoard = document.getElementById('computerScore');
 
   rockButton.addEventListener("click", function(){
-      userChoice = "rock";
-      if (compChoice === "rock") {
-      console.log("Computer chose rock, it's a draw");
+
+    userChoice = "rock";
+    var randomIndex = Math.floor(Math.random() * choices.length);
+    compChoice = choices[randomIndex];
+
+    if (compChoice === "rock") {
+      status.innerHTML = "Computer chose rock, it's a draw";
     } else if (compChoice === "paper") {
-      console.log("Computer chose paper, computer wins");
-      } else if (compChoice === "scissors") {
-      console.log("Computer chose scissors, user wins");
-      }
-    });
+      status.innerHTML = "Computer chose paper, computer wins";
+      compScore++;
+    } else if (compChoice === "scissors") {
+      status.innerHTML = "Computer chose scissors, user wins";
+      userScore++;
+    }
+
+    userScoreBoard.innerHTML = userScore;
+    computerScoreBoard.innerHTML = compScore;
+  });
 
   paperButton.addEventListener("click", function(){
       userChoice = "paper";
       if (compChoice === "paper") {
-      console.log("Computer chose paper, it's a draw");
-    } else if (compChoice === "scissors") {
-      console.log("Computer chose scissors, computer wins");
-    } else if (compChoice === "rock") {
-      console.log("Computer chose rock, user wins");
+        status.innerHTML = "Computer chose paper, it's a draw";
+      } else if (compChoice === "scissors") {
+        status.innerHTML = "Computer chose scissors, computer wins";
+        compScore++;
+      } else if (compChoice === "rock") {
+        status.innerHTML = "Computer chose rock, user wins";
+        userScore++;
       }
+
+      userScoreBoard.innerHTML = userScore;
+      computerScoreBoard.innerHTML = compScore;
     });
 
   scissorsButton.addEventListener("click", function(){
       userChoice = "scissors";
       if (compChoice === "scissors") {
-      console.log("Computer chose scissors, it's a draw");
-    } else if (compChoice === "rock") {
-      console.log("Computer chose rock, computer wins");
-    } else if (compChoice === "paper") {
-      console.log("Computer chose paper, user wins");
+        status.innerHTML = "Computer chose scissors, it's a draw";
+      } else if (compChoice === "rock") {
+        status.innerHTML = "Computer chose rock, computer wins";
+        compScore++;
+      } else if (compChoice === "paper") {
+        status.innerHTML = "Computer chose paper, user wins";
+        userScore++;
       }
+
+      userScoreBoard.innerHTML = userScore;
+      computerScoreBoard.innerHTML = compScore;
     });
+
+
 
   });
