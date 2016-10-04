aa3f183044f2b06cef5318b80d99b69aa4b0106d
Jane Maguire <jane.m.maguire@gmail.com>
2016-10-04 17:30:56 +0100

    Added win logic in if else statement

--- janemaguire/w02d02/rock-paper-scissors/js/app.js
+++ janemaguire/w02d02/rock-paper-scissors/js/app.js
@@ -3,16 +3,42 @@ document.addEventListener("DOMContentLoaded", function () {
   var rockButton = document.getElementById("rock");
   var paperButton = document.getElementById("paper");
   var scissorsButton = document.getElementById("scissors");
+  var userChoice = "";
+  var choices = ["rock", "paper", "scissors"];
+  var randomIndex = Math.floor(Math.random() * choices.length);
+  var compChoice = choices[randomIndex];
 
   rockButton.addEventListener("click", function(){
-      console.log("rock clicked");
+      userChoice = "rock";
+      if (compChoice === "rock") {
+      console.log("Computer chose rock, it's a draw");
+    } else if (compChoice === "paper") {
+      console.log("Computer chose paper, computer wins");
+      } else if (compChoice === "scissors") {
+      console.log("Computer chose scissors, user wins");
+      }
     });
 
   paperButton.addEventListener("click", function(){
-      console.log("paper clicked");
+      userChoice = "paper";
+      if (compChoice === "paper") {
+      console.log("Computer chose paper, it's a draw");
+    } else if (compChoice === "scissors") {
+      console.log("Computer chose scissors, computer wins");
+    } else if (compChoice === "rock") {
+      console.log("Computer chose rock, user wins");
+      }
     });
 
   scissorsButton.addEventListener("click", function(){
-      console.log("scissors clicked");
+      userChoice = "scissors";
+      if (compChoice === "scissors") {
+      console.log("Computer chose scissors, it's a draw");
+    } else if (compChoice === "rock") {
+      console.log("Computer chose rock, computer wins");
+    } else if (compChoice === "paper") {
+      console.log("Computer chose paper, user wins");
+      }
     });
-});
+
+  });
