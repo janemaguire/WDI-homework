38c0f868bceaa8452e0b73464ca252524066777f
Jane Maguire <jane.m.maguire@gmail.com>
2016-10-17 16:53:11 +0100

    Linked ejs files to layout

--- janemaguire/w04d01/election/config/routes.js
+++ janemaguire/w04d01/election/config/routes.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (req, res) => res.render('index'));
-router.get('/clinton', (req, res) => res.render('clinton'));
-router.get('/trump', (req, res) => res.render('trump'));
+router.get('/', (req, res) => res.render('../views/index'));
+router.get('/clinton', (req, res) => res.render('../views/clinton'));
+router.get('/trump', (req, res) => res.render('../views/trump'));
 
 module.exports = router;
