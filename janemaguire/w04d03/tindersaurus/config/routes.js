916977b1ac3a1226f5b22400b8a7c0a4109fc758
Jane Maguire <jane.m.maguire@gmail.com>
2016-10-19 18:52:48 +0100

    Added index, new, create controllers

--- janemaguire/w04d03/tindersaurus/config/routes.js
+++ janemaguire/w04d03/tindersaurus/config/routes.js
@@ -4,21 +4,7 @@ const Dinos = require('../models/dinos');
 const dinosController = require('../controllers/dinosController.js');
 
 router.get('/dinos', dinosController.index);
-
-router.get('/dinos', (req, res) => {
-	Dinos.find({}, (err, dinos) => {
-		if(err) return res.status(500).send("500: Server Error");
-		res.render('dinos/index', { dinos });
-	});
-});
-
-router.get('/dinos/new', (req, res) => res.render('dinos/new'));
-
-router.post('/dinos', (req, res) => {
-    Dino.create(req.body.dino, (err, dino) => {
-        if(err) return res.status(500).send("500: Server Error");
-        res.redirect(301, '/dinos');
-    });
-});
+router.get('/dinos/new', dinosController.new);
+router.post('/dinos', dinosController.create);
 
 module.exports = router;
--- janemaguire/w04d03/tindersaurus/controllers/dinosController.js
+++ janemaguire/w04d03/tindersaurus/controllers/dinosController.js
@@ -1,5 +1,6 @@
 const Dino = require('../models/dinos');
 
+// INDEX
 function dinosIndex(req, res) {
     Dino.find({}, (err, dinos) => {
         if(err) return res.status(500).send("500: Server Error");
@@ -7,6 +8,21 @@ function dinosIndex(req, res) {
     });
 }
 
+// NEW
+function dinosNew(req, res) {
+    res.render('dinos/new');
+}
+
+// CREATE
+function dinosCreate(req, res) {
+    Dino.create(req.body.dino, (err, dino) => {
+        if(err) return res.status(500).send("500: Server Error");
+        res.redirect(301, '/dinos');
+    });
+}
+
 module.exports = {
-    index: dinosIndex
+    index: dinosIndex,
+    new: dinosNew,
+    create: dinosCreate
 };
