00bdd95901c6e11be16f9ed6e0d4a2a7bdcda8cb
Jane Maguire <jane.m.maguire@gmail.com>
2016-10-26 17:17:02 +0100

    Set up API, set up authentication

---
+++ janemaguire/w05d03/newapi/app.js
@@ -0,0 +1,20 @@
+const express =  require ('express');
+const morgan =  require ('morgan');
+const bodyParser = require('body-parser');
+const mongoose =  require('mongoose');
+const routes = require('./config/routes');
+const app = express();
+const port = process.env.PORT || 8000;
+
+mongoose.connect('mongodb://localhost/newapi');
+
+app.use(express.static(`${__dirname}/public`));
+app.use(morgan('dev'));
+
+app.get(`/`,(req,res) => res.render('index'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended:true }));
+
+app.use('/', routes);
+
+app.listen(port,() => console.log(`running on port: ${port}`));
