cd691557efd55f80654f53fa946fa87e1f1aee28
Jane Maguire <jane.m.maguire@gmail.com>
2016-11-07 18:31:54 +0000

    Wrote get requests on /projects/:id, wrote post requests on /projects

--- janemaguire/w07d01/starter-code/test/projects_tests.js
+++ janemaguire/w07d01/starter-code/test/projects_tests.js
@@ -7,7 +7,7 @@ const app = require('../app');
 const api = supertest(app);
 const Project = require('../models/project');
 
-// Projects Index
+
 describe("GET /projects", () => {
 
   before(done => {
@@ -35,5 +35,66 @@ describe("GET /projects", () => {
         done();
       });
   });
+
+  describe("GET /projects/:id", () => {
+
+    before(done => {
+      Project.collection.drop();
+      Project.create({
+        title: 'Excellente',
+        github: 'http://github.com/excellente',
+        url: 'http://excellente.herokuapp.com',
+        users: ["5820b539ea481a8708720eae"]
+      }, done);
+    });
+
+    it("should return a 200 response", (done) => {
+      api.get('/projects')
+        .set('Accept', 'application/json')
+        .expect(200, done);
+    });
+
+    it("should return an object", (done) => {
+      api.get('/projects')
+        .set('Accept', 'applications/json')
+        .end((err, res) => {
+          expect(res.body[0]).to.have.property('title');
+          expect(res.body[0]).to.have.property('github');
+          expect(res.body[0]).to.have.property('url');
+          expect(res.body[0]).to.have.property('users');
+          done();
+        });
+    });
+  });
+});
+
+describe("POST /projects", () => {
+
+  it('should return a 201 response', (done) => {
+    api.post('/projects')
+      .set('Accept', 'application/json')
+      .send({
+        title: 'Excellente',
+        github: 'http://github.com/excellente',
+        url: 'http://excellente.herokuapp.com',
+        users: ["5820b539ea481a8708720eae"]
+      })
+      .expect(201, done);
+    });
+
+    it('should return an object', (done) => {
+      api.post('/projects')
+        .set('Accept', 'application/json')
+        .send({
+          title: 'Excellente',
+          github: 'http://github.com/excellente',
+          url: 'http://excellente.herokuapp.com',
+          users: ["5820b539ea481a8708720eae"]
+        })
+        .end((err, res) => {
+        expect(res.body).to.be.an('object');
+        done();
+      });
+    });
 
 });
