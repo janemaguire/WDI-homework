c187f91e942fd8672bb1c37a4026edc86cadc537
Jane Maguire <jane.m.maguire@gmail.com>
2016-11-09 19:37:19 +0000

    Added delete

--- janemaguire/w07d03/criminals-app/public/js/criminalsController.js
+++ janemaguire/w07d03/criminals-app/public/js/criminalsController.js
@@ -1,27 +1,36 @@
 angular.module('criminalsApp')
-  .controller('CriminalsController', CriminalsController);
+.controller('CriminalsController', CriminalsController);
 
 CriminalsController.$inject = ['$http'];
 
 function CriminalsController($http) {
+  
   const criminals = this;
   criminals.all = [];
   criminals.addCriminal = addCriminal;
 
+  criminals.removeCriminal = removeCriminal;
   function addCriminal() {
     $http.post('criminals', criminals.newCriminal)
-      .then((res) => {
-        criminals.all.push(res.data);
-        // criminals.newCriminal = {};
-        console.log(criminals.newCriminal);
-      });
+    .then((res) => {
+      criminals.all.push(res.data);
+      // criminals.newCriminal = {};
+      console.log(criminals.newCriminal);
+    });
   }
 
   function getCriminals() {
     $http.get('/criminals')
-      .then((res) => {
-        criminals.all = res.data;
-      });
+    .then((res) => {
+      criminals.all = res.data;
+    });
+  }
+
+  function removeCriminal(id, $index) {
+    $http.delete(`/criminals/${id}`)
+    .then(() => {
+      criminals.all.splice($index, 1);
+    });
   }
   getCriminals();
 }
