9fe9db118e07d4dca902b0314079978cff9afb0d
Jane Maguire <jane.m.maguire@gmail.com>
2016-11-10 17:18:03 +0000

    Added states and navbar

--- janemaguire/w07d04/todos/js/app.js
+++ janemaguire/w07d04/todos/js/app.js
@@ -1,2 +1,18 @@
 angular
-	.module('todoApp', ['ui.router']);
+	.module('todoApp', ['ui.router'])
+  .config(MainRouter);
+
+MainRouter.$inject = ['$stateProvider','$urlRouterProvider'];
+
+function MainRouter($stateProvider,$urlRouterProvider) {
+  $stateProvider
+    .state('home', {
+      url: '/',
+      templateUrl: 'home.html'
+    })
+    .state('archive', {
+      url: '/',
+      templateUrl: 'archive.html'
+    });
+  $urlRouterProvider.otherwise('/');
+}
