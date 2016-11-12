be09f50831909ab20a301a2ef774db81c6957af5
Jane Maguire <jane.m.maguire@gmail.com>
2016-11-12 15:38:57 +0000

    Set up API, finished index

--- janemaguire/w07d05/starter-code/config/routes.js
+++ janemaguire/w07d05/starter-code/config/routes.js
@@ -1,5 +1,18 @@
 const router = require('express').Router();
+const birdsController = require('../controllers/birds');
+const authController = require('../controllers/auth');
 
-// Add user routes or other resource routes here
+router
+  .post('/login', authController.login)
+  .post('/register', authController.register);
+
+router.route('/birds')
+  .get(birdsController.index)
+  .post(birdsController.create);
+
+router.route('/birds/:id')
+  .get(birdsController.show)
+  .put(birdsController.update)
+  .delete(birdsController.delete);
 
 module.exports = router;
