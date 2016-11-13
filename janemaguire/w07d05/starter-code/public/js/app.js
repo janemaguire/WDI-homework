e28d1eaf851061a6419ad19ed56304f257fdee10
Jane Maguire <jane.m.maguire@gmail.com>
2016-11-13 18:44:08 +0000

    Added styling

--- janemaguire/w07d05/starter-code/public/js/app.js
+++ janemaguire/w07d05/starter-code/public/js/app.js
@@ -5,11 +5,26 @@ angular
 Router.$inject = ['$stateProvider', '$urlRouterProvider'];
 function Router($stateProvider, $urlRouterProvider) {
   $stateProvider
-  .state('birdsIndex', {
-    url: '/birds',
-    templateUrl: '/templates/birdsIndex.html',
-    controller: 'BirdsIndexController as birdsIndex'
-  });
+    .state('birdsIndex', {
+      url: '/birds',
+      templateUrl: '/templates/birdsIndex.html',
+      controller: 'BirdsIndexController as birdsIndex'
+    })
+    .state('birdsNew', {
+      url: '/birds/new',
+      templateUrl: '/templates/birdsNew.html',
+      controller: 'BirdsNewController as birdsNew'
+    })
+    .state('birdsShow', {
+      url: '/birds/:id',
+      templateUrl: '/templates/birdsShow.html',
+      controller: 'BirdsShowController as birdsShow'
+    })
+    .state('birdsEdit', {
+      url: '/birds/:id/edit',
+      templateUrl: '/templates/birdsEdit.html',
+      controller: 'BirdsEditController as birdsEdit'
+    });
 
   $urlRouterProvider.otherwise('/birds');
 }
--- janemaguire/w07d05/starter-code/public/js/controllers/birds.js
+++ janemaguire/w07d05/starter-code/public/js/controllers/birds.js
@@ -1,13 +1,55 @@
 angular.module('birdsApp')
-    .controller('BirdsIndexController', BirdsIndexController);
-
-function BirdsIndexController(){
-  const birdsIndex = this;
-}
+    .controller('BirdsIndexController', BirdsIndexController)
+    .controller('BirdsNewController', BirdsNewController)
+    .controller('BirdsShowController', BirdsShowController)
+    .controller('BirdsEditController', BirdsEditController);
 
 BirdsIndexController.$inject = ['Bird'];
 function BirdsIndexController(Bird){
   const birdsIndex = this;
 
   birdsIndex.all = Bird.query();
+}
+
+BirdsNewController.$inject = ['Bird', '$state'];
+function BirdsNewController(Bird, $state) {
+  const birdsNew = this;
+  birdsNew.bird = {};
+
+  function createBird() {
+    Bird.save(birdsNew.bird, () => {
+      $state.go('birdsIndex');
+    });
+
+  }
+  birdsNew.create = createBird;
+}
+
+BirdsShowController.$inject = ['Bird', '$state'];
+function BirdsShowController(Bird, $state) {
+  const birdsShow = this;
+
+  birdsShow.bird = Bird.get($state.params);
+
+  function deleteBird() {
+    birdsShow.bird.$remove(() => {
+      $state.go('birdsIndex');
+    });
+  }
+
+  birdsShow.delete = deleteBird;
+}
+
+BirdsEditController.$inject = ['Bird', '$state'];
+function BirdsEditController(Bird, $state) {
+  const birdsEdit = this;
+
+  birdsEdit.bird = Bird.get($state.params);
+
+  function updateBird() {
+    birdsEdit.bird.$update(() => {
+      $state.go('birdsShow', $state.params);
+    });
+  }
+  birdsEdit.update = updateBird;
 }
