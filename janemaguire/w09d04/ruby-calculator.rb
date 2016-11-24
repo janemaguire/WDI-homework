ff34f4dac1a2b1742ee1ce10b4296285f814c122
Jane Maguire <jane.m.maguire@gmail.com>
2016-11-24 17:15:36 +0000

    Added square root

--- janemaguire/w09d04/ruby-calculator.rb
+++ janemaguire/w09d04/ruby-calculator.rb
@@ -3,21 +3,27 @@ while calc do
 
   p "Enter your first number and hit return"
   input_one = gets.to_f
-  p "Choose from +, -, *, / or ^ and hit return"
+  p "Choose from +, -, *, /, ^ or sq and hit return"
   operator = gets.chomp
-  p "Enter your second number and hit return"
-  input_two = gets.to_f
 
-  if operator == "+"
-    puts "Your answer is " + (input_one + input_two).to_s
-  elsif operator == "-"
-    puts "Your answer is " + (input_one - input_two).to_s
-  elsif operator == "*"
-    puts "Your answer is " + (input_one * input_two).to_s
-  elsif operator == "/"
-    puts "Your answer is " + (input_one / input_two).to_s
-  elsif operator == "^"
-    puts "Your answer is " + (input_one ** input_two).to_s
+  if operator == "sq"
+    puts Math.sqrt(input_one).to_s
+  else
+
+    p "Enter your second number and hit return"
+    input_two = gets.to_f
+
+    if operator == "+"
+      puts "Your answer is " + (input_one + input_two).to_s
+    elsif operator == "-"
+      puts "Your answer is " + (input_one - input_two).to_s
+    elsif operator == "*"
+      puts "Your answer is " + (input_one * input_two).to_s
+    elsif operator == "/"
+      puts "Your answer is " + (input_one / input_two).to_s
+    elsif operator == "^"
+      puts "Your answer is " + (input_one ** input_two).to_s
+    end
   end
 
 puts "Do another calculation? (y/n)"
