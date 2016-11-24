e5d70750c4ddf9ce832dbb0a8dc294f9c70aeb9c
Jane Maguire <jane.m.maguire@gmail.com>
2016-11-24 17:00:50 +0000

    Added to the power of

--- janemaguire/w09d04/ruby-calculator.rb
+++ janemaguire/w09d04/ruby-calculator.rb
@@ -3,7 +3,7 @@ while calc do
 
   p "Enter your first number and hit return"
   input_one = gets.to_f
-  p "Choose from +, -, * or / and hit return"
+  p "Choose from +, -, *, / or ^ and hit return"
   operator = gets.chomp
   p "Enter your second number and hit return"
   input_two = gets.to_f
@@ -16,6 +16,8 @@ while calc do
     puts "Your answer is " + (input_one * input_two).to_s
   elsif operator == "/"
     puts "Your answer is " + (input_one / input_two).to_s
+  elsif operator == "^"
+    puts "Your answer is " + (input_one ** input_two).to_s
   end
 
 puts "Do another calculation? (y/n)"
