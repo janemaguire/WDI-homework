8c3d09842e838451be1db617950528dac3991fca
Jane Maguire <jane.m.maguire@gmail.com>
2016-11-24 17:27:18 +0000

    Added weight on moon

--- janemaguire/w09d04/ruby-calculator.rb
+++ janemaguire/w09d04/ruby-calculator.rb
@@ -3,11 +3,13 @@ while calc do
 
   p "Enter your first number and hit return"
   input_one = gets.to_f
-  p "Choose from +, -, *, /, ^ or sq and hit return"
+  p "Choose from +, -, *, /, ^, sq or moon and hit return"
   operator = gets.chomp
 
   if operator == "sq"
     puts Math.sqrt(input_one).to_s
+  elsif operator == "moon"
+    puts (input_one) * (0.2)
   else
 
     p "Enter your second number and hit return"
