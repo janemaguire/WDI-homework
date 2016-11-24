1eacfbc968d00c1f1419a321adf19c4f59413f6b
Jane Maguire <jane.m.maguire@gmail.com>
2016-11-24 16:51:15 +0000

    Added loop

--- janemaguire/w09d04/ruby-calculator.rb
+++ janemaguire/w09d04/ruby-calculator.rb
@@ -1,16 +1,25 @@
-p "Enter your first number and hit return"
-input_one = gets.to_i
-p "Choose from +, -, * or / and hit return"
-operator = gets.chomp
-p "Enter your second number and hit return"
-input_two = gets.to_i
+calc = true
+while calc do
 
-if operator == "+"
-  puts "Your answer is " + (input_one + input_two).to_s
-elsif operator == "-"
-  puts "Your answer is " + (input_one - input_two).to_s
-elsif operator == "*"
-  puts "Your answer is " + (input_one * input_two).to_s
-elsif operator == "/"
-  puts "Your answer is " + (input_one / input_two).to_s
+  p "Enter your first number and hit return"
+  input_one = gets.to_f
+  p "Choose from +, -, * or / and hit return"
+  operator = gets.chomp
+  p "Enter your second number and hit return"
+  input_two = gets.to_f
+
+  if operator == "+"
+    puts "Your answer is " + (input_one + input_two).to_s
+  elsif operator == "-"
+    puts "Your answer is " + (input_one - input_two).to_s
+  elsif operator == "*"
+    puts "Your answer is " + (input_one * input_two).to_s
+  elsif operator == "/"
+    puts "Your answer is " + (input_one / input_two).to_s
+  end
+
+puts "Do another calculation? (y/n)"
+calc = gets.chomp == 'y'
 end
+
+puts "Ok, bye then"
