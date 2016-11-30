ae83adb4430812e3b8717530006921e0de9d812a
Jane Maguire <jane.m.maguire@gmail.com>
2016-11-30 17:16:50 +0000

    Displayed images, added continents

--- janemaguire/w10d03/countries/db/schema.rb
+++ janemaguire/w10d03/countries/db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161130155854) do
+ActiveRecord::Schema.define(version: 20161130164507) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "continents", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "countries", force: :cascade do |t|
     t.string   "name"
