9e3646d6c23e829a1c1db8895dbc716a1fc9eca5
Jane Maguire <jane.m.maguire@gmail.com>
2016-12-02 16:10:53 +0000

    Sessions controller

--- janemaguire/w10d05/yass-queen/config/routes.rb
+++ janemaguire/w10d05/yass-queen/config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
+
   root 'queens#index'
   get '/register', to: 'users#new'
   post '/register', to: 'users#create'
+
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
 
   resources :queens
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
