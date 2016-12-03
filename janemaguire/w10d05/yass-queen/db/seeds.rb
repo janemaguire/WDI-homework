d62c92a8ab8e1a42a595d3c7b97d59e0766938e7
Jane Maguire <jane.m.maguire@gmail.com>
2016-12-03 15:57:40 +0000

    Added seed data

--- janemaguire/w10d05/yass-queen/db/seeds.rb
+++ janemaguire/w10d05/yass-queen/db/seeds.rb
@@ -16,7 +16,7 @@ rvt = Venue.create!(
   opened: "1863"
 )
 
-panti = Venue.create!(
+pantibar = Venue.create!(
   name:"PantiBar",
   image: "http://www.outmag.co.uk/site/images/stories/royalvauxhalltavern2.png",
   city: "Dublin",
@@ -32,8 +32,23 @@ pyramid = Venue.create!(
   opened: "1979"
 )
 
-lily = Queen.create!(
+cruz = Venue.create!(
+  name:"Cruz 101",
+  image: "http://www.outmag.co.uk/site/images/stories/royalvauxhalltavern2.png",
+  city: "Manchester",
+  country: "UK",
+  opened: "1992"
+)
 
+jacques = Venue.create!(
+  name:"Jacque’s Cabaret",
+  image: "http://www.outmag.co.uk/site/images/stories/royalvauxhalltavern2.png",
+  city: "Boston",
+  country: "USA",
+  opened: "1938"
+)
+
+lily = Queen.create!(
   name: "Lily Savage",
   image: "http://images.npg.org.uk/800_800/9/7/mw15297.jpg",
   description: "",
@@ -42,68 +57,81 @@ lily = Queen.create!(
   catchphrases: "If I wanted your opinion, I'd slap it outta ya",
   website: ""
 )
-
 lily.venues << Venue.find_by_name(rvt.name)
-# {
-#   name: "Myra DuBois",
-#   image: "http://gscene.com/wp-content/uploads/2015/10/Myra-dubois_MG_7534.jpg",
-#   description: "",
-#   nationality: "British",
-#   hometown: "Rotherham",
-#   catchphrases: "",
-#   website: "http://www.myradubois.co.uk/"
-#   },
-#   {
-#   name: "Jujubee",
-#   image: "http://soundnh.com/wp-content/uploads/2015/01/STAGEdrag_jujubee_from_rupauls_drag_race_courtesy_photo.jpg",
-#   description: "",
-#   nationality: "American",
-#   hometown: "Boston",
-#   catchphrases: "I feel like I'm super hero diva and my power is glitter, I want fried chicken now",
-#   website: "http://www.jujubeeonline.com/"
-#   },
-#   {
-#   name: "Tina C",
-#   image: "https://ichef.bbci.co.uk/images/ic/640x360/p01l83bq.jpg",
-#   description: "",
-#   nationality: "American",
-#   hometown: "Tennessee",
-#   catchphrases: "If you can't live without me, why weren't you dead when I met you?,",
-#   website: "http://www.christophergreen.net/tina-c/"
-#   },
-#   {
-#   name: "Timberlina",
-#   image: "http://www.qxmagazine.com/wp-content/uploads/2011/06/TimberlinabyMikeKear2011-copy.jpg",
-#   description: "",
-#   nationality: "British",
-#   hometown: "London",
-#   catchphrases: "It's a rollover!",
-#   website: "http://www.timberlina.co.uk/"
-#   },
-#   {
-#   name: "RuPaul",
-#   image: "http://www.adweek.com/files/rupaul-cover-hed-2014.jpg",
-#   description: "",
-#   nationality: "American",
-#   hometown: "San Diego",
-#   catchphrases: "If you don't love yourself, how in the hell you gonna love somebody else?, We're born naked, and the rest is drag.",
-#   website: "http://www.rupaul.com/"
-#   },
-#   {
-#   name: "Adore Delano",
-#   image: "https://s-media-cache-ak0.pinimg.com/originals/ca/31/17/ca31171ff15dd8c91fa2948a7129f4b9.jpg",
-#   description: "",
-#   nationality: "American",
-#   hometown: "Glendora",
-#   catchphrases: "Party!, I wanted to show my hog body, I don't belong here",
-#   website: "http://www.adoredelano.com/"
-#   },
-#   {
-#   name: "Liquorice Black",
-#   image: "http://scontent.cdninstagram.com/t51.2885-15/s480x480/e35/c160.0.640.640/13398578_265786943777690_1801630703_n.jpg?ig_cache_key=MTI3NjY5MDIyMjEzNzk2NTEwNA%3D%3D.2.c",
-#   description: "",
-#   nationality: "British",
-#   hometown: "Manchester",
-#   catchphrases: "",
-#   website: "https://www.instagram.com/liquorice_black/"
-#     }])
+
+myra = Queen.create!(
+  name: "Myra DuBois",
+  image: "http://gscene.com/wp-content/uploads/2015/10/Myra-dubois_MG_7534.jpg",
+  description: "",
+  nationality: "British",
+  hometown: "Rotherham",
+  catchphrases: "",
+  website: "http://www.myradubois.co.uk/"
+)
+myra.venues << Venue.find_by_name(rvt.name)
+
+jujubee = Queen.create!(
+  name: "Jujubee",
+  image: "http://soundnh.com/wp-content/uploads/2015/01/STAGEdrag_jujubee_from_rupauls_drag_race_courtesy_photo.jpg",
+  description: "",
+  nationality: "American",
+  hometown: "Boston",
+  catchphrases: "I feel like I'm super hero diva and my power is glitter, I want fried chicken now",
+  website: "http://www.jujubeeonline.com/"
+)
+jujubee.venues << Venue.find_by_name(jacques.name)
+
+tina = Queen.create!(
+  name: "Tina C",
+  image: "https://ichef.bbci.co.uk/images/ic/640x360/p01l83bq.jpg",
+  description: "",
+  nationality: "American",
+  hometown: "Tennessee",
+  catchphrases: "If you can't live without me, why weren't you dead when I met you?,",
+  website: "http://www.christophergreen.net/tina-c/"
+)
+tina.venues << Venue.find_by_name(jacques.name)
+
+tim = Queen.create!(
+  name: "Timberlina",
+  image: "http://www.qxmagazine.com/wp-content/uploads/2011/06/TimberlinabyMikeKear2011-copy.jpg",
+  description: "",
+  nationality: "British",
+  hometown: "London",
+  catchphrases: "It's a rollover!",
+  website: "http://www.timberlina.co.uk/"
+)
+tim.venues << Venue.find_by_name(rvt.name)
+
+ru = Queen.create!(
+  name: "RuPaul",
+  image: "http://www.adweek.com/files/rupaul-cover-hed-2014.jpg",
+  description: "",
+  nationality: "American",
+  hometown: "San Diego",
+  catchphrases: "If you don't love yourself, how in the hell you gonna love somebody else?, We're born naked, and the rest is drag.",
+  website: "http://www.rupaul.com/"
+)
+ru.venues << Venue.find_by_name(pyramid.name)
+
+liq = Queen.create!(
+  name: "Liquorice Black",
+  image: "http://scontent.cdninstagram.com/t51.2885-15/s480x480/e35/c160.0.640.640/13398578_265786943777690_1801630703_n.jpg?ig_cache_key=MTI3NjY5MDIyMjEzNzk2NTEwNA%3D%3D.2.c",
+  description: "",
+  nationality: "British",
+  hometown: "Manchester",
+  catchphrases: "",
+  website: "https://www.instagram.com/liquorice_black/"
+  )
+liq.venues << Venue.find_by_name(cruz.name)
+
+panti = Queen.create!(
+  name: "Panti Bliss",
+  image: "http://www.abc.net.au/news/image/7208530-1x1-700x700.jpg",
+  description: "",
+  nationality: "Irish",
+  hometown: "Dublin",
+  catchphrases: "",
+  website: "https://twitter.com/PantiBliss"
+  )
+panti.venues << Venue.find_by_name(pantibar.name)
